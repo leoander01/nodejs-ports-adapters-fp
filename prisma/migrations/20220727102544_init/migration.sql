5d5c23e074833265600dec3e3988576afbe80e71
leoander01 <leoanderneves8@outlook.com>
2022-07-27 07:34:50 -0300

    Add index to prisma schema

--- prisma/migrations/20220727102544_init/migration.sql
+++ prisma/migrations/20220727102544_init/migration.sql
@@ -13,6 +13,14 @@ CREATE TABLE "User" (
 );
 
 -- CreateTable
+CREATE TABLE "Follower" (
+    "userId" TEXT NOT NULL,
+    "followingId" TEXT NOT NULL,
+
+    CONSTRAINT "Follower_pkey" PRIMARY KEY ("userId","followingId")
+);
+
+-- CreateTable
 CREATE TABLE "Tag" (
     "id" TEXT NOT NULL,
     "name" TEXT NOT NULL,
@@ -43,10 +51,25 @@ CREATE TABLE "_ArticleToTag" (
 );
 
 -- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_username_key" ON "User"("username");
+
+-- CreateIndex
+CREATE INDEX "User_email_username_idx" ON "User"("email", "username");
+
+-- CreateIndex
 CREATE UNIQUE INDEX "_ArticleToTag_AB_unique" ON "_ArticleToTag"("A", "B");
 
 -- CreateIndex
 CREATE INDEX "_ArticleToTag_B_index" ON "_ArticleToTag"("B");
+
+-- AddForeignKey
+ALTER TABLE "Follower" ADD CONSTRAINT "Follower_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Follower" ADD CONSTRAINT "Follower_followingId_fkey" FOREIGN KEY ("followingId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "Article" ADD CONSTRAINT "Article_authorId_fkey" FOREIGN KEY ("authorId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
